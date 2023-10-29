7aa96c764a215669c8b24fa4e57765e4920c93f9
WayneChen158 <ychen168@seas.upenn.edu>
2023-10-29 10:21:17 -0700

    Initial project start

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+
+    return (<aside>
+        <h2>Inventory Management System</h2>
+        <div>
+            <button>
+                + Add item
+            </button>
+        </div>
+    </aside>
+    )
+}
