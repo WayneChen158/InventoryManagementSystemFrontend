28a45a456b7df0c81fd70268f439300bfa8fa0ce
Xinhao <songxinhao@hotmail.com>
2023-11-02 18:01:54 -0700

    Fix search item bug

--- template/src/sections/inventory/utils.js
+++ template/src/sections/inventory/utils.js
@@ -52,7 +52,7 @@ export function applyFilter({ inputData, comparator, filterName }) {
 
   if (filterName) {
     inputData = inputData.filter(
-      (user) => user.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
+      (item) => item.description.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
     );
   }
 
