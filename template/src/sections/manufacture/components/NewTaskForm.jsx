d19394d23abd7e857f0c2e41d2565f37dc1b3aea
WayneChen158 <ychen168@seas.upenn.edu>
2023-11-08 09:15:23 -0800

    1. be able to fetch recipe list 2. disable 'Submit' if something is not enough 3. New layout of form

--- template/src/sections/manufacture/components/NewTaskForm.jsx
+++ template/src/sections/manufacture/components/NewTaskForm.jsx
@@ -6,19 +6,25 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import { Card, Box, Button, TextField, Select, MenuItem, InputLabel, Stack, Grid } from '@mui/material';
+import { Card, Box, Button, TextField, Select,
+    MenuItem, InputLabel, Stack, Grid, TableHead, 
+    TableRow, TableCell, TableBody, Table, Paper, TableContainer } from '@mui/material';
 
 import { config } from '../../../config';
+import Label from '../../../components/label';
 
 export default function NewTaskForm() {
   const [type, setType] = useState('component');
   const [componentId, setComponentId] = useState(0);
   const [productId, setProductId] = useState(0);
   const [scale, setScale] = useState(0);
+
   const [isChecking, setIsChecking] = useState(false);
+  const [allPass, setAllPass] = useState(false);
 
   const [productLst, setProductLst] = useState([]);
   const [componentLst, setComponentLst] = useState([]);
+  const [itemLst, setItemLst] = useState([]);
 
   useEffect(() => {
     fetch(`http://${config.server_host}:${config.server_port}/api/products`, {
@@ -70,8 +76,35 @@ export default function NewTaskForm() {
       .catch((error) => {
         console.error('There was a problem with the fetch operation:', error);
       });
-  }
-}, [productId]);
+    }}, [productId]);
+
+    useEffect(() => {
+        if (componentId !== 0 && isChecking) {
+        fetch(`http://${config.server_host}:${config.server_port}/api/components/manufacture/${componentId}?scale=${scale}`, {
+            method: 'GET',
+        })
+            .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+            })
+            .then((resdata) => {
+                setItemLst(resdata);
+            })
+            .catch((error) => {
+            console.error('There was a problem with the fetch operation:', error);
+            });
+        }}, [componentId, isChecking, scale]);
+
+    useEffect(() => {
+        // Check if any item in itemLst has hasEnoughInStock as false
+        const anyItemNotInStock = itemLst.some((item) => !item.hasEnoughInStock);
+        
+        // Set allPass to the negation of anyItemNotInStock
+        setAllPass(!(anyItemNotInStock || itemLst.length === 0));
+        }, [itemLst]);
+
 
   const handleTypeChange = (event) => {
     setType(event.target.value);
@@ -95,7 +128,6 @@ export default function NewTaskForm() {
 
   const handleCheckInventory = () => {
     setIsChecking(true);
-    console.log(isChecking);
   };
 
   const handleSubmit = () => {
@@ -108,85 +140,182 @@ export default function NewTaskForm() {
 
   return (
     <Grid container spacing={0.5} justifyContent="center">
-    <Grid item xs={6}>
-    <Card style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '100%' }}>
-        <Stack>
-            <Box>
-                <h2>Create a new manufacture task</h2>
-            </Box>
-            <Box style={{padding: '10px 0 0 0'}}>
-                <FormControl>
-                <FormLabel>Task Type</FormLabel>
-                <RadioGroup row value={type} onChange={handleTypeChange}>
-                    <FormControlLabel value="component" control={<Radio />} label="Component" />
-                    <FormControlLabel value="product" control={<Radio />} label="Product" />
-                </RadioGroup>
-                </FormControl>
-            </Box>
+    {isChecking && <Grid item xs = {12}>
+    <Card style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '120px' }}>
+        <Box style={{padding: '10px 0 0 0'}}>
+            <FormControl>
+            <FormLabel>Task Type</FormLabel>
+            <RadioGroup column = 'true' value={type} onChange={handleTypeChange}>
+                <FormControlLabel value="component" control={<Radio />} label="Component" />
+                <FormControlLabel value="product" control={<Radio />} label="Product" />
+            </RadioGroup>
+            </FormControl>
+        </Box>
 
-            <Stack style={{padding: '10px 0 0 0'}}>
-                {type === 'component' && (
-                    <>
-                    <FormControl style={{padding: '10px 0 0 0'}}>
-                        <InputLabel>Choose Product</InputLabel>
-                        <Select value={productId} onChange={handleProductChange}>
-                            <MenuItem key = {0} value={0}>None</MenuItem>
-                            {productLst.map((product) => (
-                                <MenuItem key={product.productId} value={product.productId}>{product.productName}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                    <FormControl style={{padding: '10px 0 0 0'}}>
-                        <InputLabel>Choose Component</InputLabel>
-                        <Select value={componentId} onChange={handleComponentChange}>
-                            <MenuItem key = {0} value={0}>None</MenuItem>
-                            {componentLst.map((component) => (
-                                <MenuItem key={component.componentId} value={component.componentId}>{component.componentName}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                    </>
-                )}
+        {type === 'component' && (
+            <>
+            <FormControl style={{padding: '10px 10px 0 0', width: '200px'}}>
+                <InputLabel>Choose Product</InputLabel>
+                <Select value={productId} onChange={handleProductChange}>
+                    <MenuItem key = {0} value={0}>None</MenuItem>
+                    {productLst.map((product) => (
+                        <MenuItem key={product.productId} value={product.productId}>{product.productName}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <FormControl style={{padding: '10px 10px 0 0', width: '200px'}}>
+                <InputLabel>Choose Component</InputLabel>
+                <Select value={componentId} onChange={handleComponentChange}>
+                    <MenuItem key = {0} value={0}>None</MenuItem>
+                    {componentLst.map((component) => (
+                        <MenuItem key={component.componentId} value={component.componentId}>{component.componentName}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            </>
+        )}
 
-                {type === 'product' && (
-                    <FormControl>
-                        <InputLabel>Choose Product</InputLabel>
-                        <Select value={productId} onChange={handleProductChange}>
-                            <MenuItem key = {0} value={0}>None</MenuItem>
-                            {productLst.map((product) => (
-                                <MenuItem key={product.productId} value={product.productId}>{product.productName}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                )}
-            </Stack>
+        {type === 'product' && (
+            <FormControl style={{padding: '10px 10px 0 0', width: '200px'}}>
+                <InputLabel>Choose Product</InputLabel>
+                <Select value={productId} onChange={handleProductChange}>
+                    <MenuItem key = {0} value={0}>None</MenuItem>
+                    {productLst.map((product) => (
+                        <MenuItem key={product.productId} value={product.productId}>{product.productName}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        )}
+
+    <Box style={{padding: '10px 0 0 0', width: '150px'}}>
+        <TextField
+            label="Scale"
+            type="number"
+            value={scale}
+            onChange={handleScaleChange}
+        />
+    </Box>
+
+    <Stack style={{padding: '10px 0 0 10px'}}>
+        <Button variant="contained" onClick={handleCheckInventory} >
+            Check Inventory
+        </Button>
         
-            <Box style={{padding: '10px 0 0 0'}}>
-                <TextField
-                    label="Scale"
-                    type="number"
-                    value={scale}
-                    onChange={handleScaleChange}
-                />
-            </Box>
-
-            <Box style={{padding: '10px 0 0 0'}}>
-                <Button variant="contained" onClick={handleCheckInventory} style={{margin: '0 10px 0 0'}}>
-                    Check Inventory
-                </Button>
-                
-                <Button variant="contained" onClick={handleSubmit} style={{margin: '0 0 0 0'}}>
-                    Submit
-                </Button>
-            </Box>
-        </Stack>
+        <Button 
+            variant="contained" 
+            onClick={handleSubmit} 
+            style={{margin: '10px 0 0 0'}}
+            disabled={!allPass}
+        >
+            Submit
+        </Button>
+    </Stack>
     </Card>
-    </Grid>
-    {isChecking && (
-      <Grid item xs>
-        <Card>
-          <h2>Coming soon</h2>
+    </Grid>}
+    
+    {!isChecking && <Grid item xs={6}>
+        <Card style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '100%' }}>
+            <Stack>
+                <Box>
+                    <h2>Create a new manufacture task</h2>
+                </Box>
+                <Box style={{padding: '10px 0 0 0'}}>
+                    <FormControl>
+                    <FormLabel>Task Type</FormLabel>
+                    <RadioGroup row value={type} onChange={handleTypeChange}>
+                        <FormControlLabel value="component" control={<Radio />} label="Component" />
+                        <FormControlLabel value="product" control={<Radio />} label="Product" />
+                    </RadioGroup>
+                    </FormControl>
+                </Box>
+
+                <Stack style={{padding: '10px 0 0 0'}}>
+                    {type === 'component' && (
+                        <>
+                        <FormControl style={{padding: '10px 0 0 0'}}>
+                            <InputLabel>Choose Product</InputLabel>
+                            <Select value={productId} onChange={handleProductChange}>
+                                <MenuItem key = {0} value={0}>None</MenuItem>
+                                {productLst.map((product) => (
+                                    <MenuItem key={product.productId} value={product.productId}>{product.productName}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <FormControl style={{padding: '10px 0 0 0'}}>
+                            <InputLabel>Choose Component</InputLabel>
+                            <Select value={componentId} onChange={handleComponentChange}>
+                                <MenuItem key = {0} value={0}>None</MenuItem>
+                                {componentLst.map((component) => (
+                                    <MenuItem key={component.componentId} value={component.componentId}>{component.componentName}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        </>
+                    )}
+
+                    {type === 'product' && (
+                        <FormControl>
+                            <InputLabel>Choose Product</InputLabel>
+                            <Select value={productId} onChange={handleProductChange}>
+                                <MenuItem key = {0} value={0}>None</MenuItem>
+                                {productLst.map((product) => (
+                                    <MenuItem key={product.productId} value={product.productId}>{product.productName}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    )}
+                </Stack>
+            
+                <Box style={{padding: '10px 0 0 0'}}>
+                    <TextField
+                        label="Scale"
+                        type="number"
+                        value={scale}
+                        onChange={handleScaleChange}
+                    />
+                </Box>
+
+                <Box style={{padding: '10px 0 0 0'}}>
+                    <Button variant="contained" onClick={handleCheckInventory} style={{margin: '0 10px 0 0'}}>
+                        Check Inventory
+                    </Button>
+                    
+                    <Button variant="contained" onClick={handleSubmit} style={{margin: '0 0 0 0'}} disabled={!allPass}>
+                        Submit
+                    </Button>
+                </Box>
+            </Stack>
         </Card>
+    </Grid>}
+    {isChecking && (
+      <Grid item xs={12}>
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+            <TableContainer sx={{ maxHeight: 800 }}>
+                <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>{type === 'component' ? 'Item Name' : 'Component Name'}</TableCell>
+                        <TableCell>Vol Per Rxn</TableCell>
+                        <TableCell>Required Amount</TableCell>
+                        <TableCell>Amount In Stock</TableCell>
+                        <TableCell>test</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {itemLst.map((row) => (
+                    <TableRow key = {row.itemName}>
+                        <TableCell>{row.itemName}</TableCell>
+                        <TableCell>{row.volPerRxn}</TableCell>
+                        <TableCell>{row.vol}</TableCell>
+                        <TableCell>
+                            <Label color={row.hasEnoughInStock ? 'success' : 'error'}>{row.hasEnoughInStock ? 'YES' : 'NO'}</Label>
+                        </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+                </Table>
+            </TableContainer>
+        </Paper>
       </Grid>
     )}
     </Grid>
