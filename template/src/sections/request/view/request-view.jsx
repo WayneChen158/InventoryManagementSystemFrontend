dfd2e1b4e92acf26aa921d33511a9b6bd6a4d8c1
Xinhao <songxinhao@hotmail.com>
2023-11-24 23:53:09 -0800

    Create new request submission form

--- template/src/sections/request/view/request-view.jsx
+++ template/src/sections/request/view/request-view.jsx
@@ -1,26 +1,51 @@
 import { useState } from 'react';
 
 import { TabList, TabPanel, TabContext } from '@mui/lab';
-import { Tab, Box, Stack, Button, Container, Typography } from '@mui/material';
+import { Tab, Box, Modal, Stack, Button, Container, Typography } from '@mui/material';
 
 import Iconify from 'src/components/iconify';
 
 import RequestPurchasePageNew from './request-view-new';
+import NewRequestForm from '../components/NewRequestForm';
 
 export default function RequestPage() {
     const [tabValue, setTabValue] = useState('new');
 
+    const [openModal, setOpenModal] = useState(false);
+
     const handleTabChange = (event, newTabValue) => {
         setTabValue(newTabValue);
+    }
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    }
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
     }
 
     return (
         <Container>
             <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
                 <Typography variant="h4">Request</Typography>
-                <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
+                
+                <Button 
+                    variant="contained" 
+                    startIcon={<Iconify icon="eva:plus-fill" />}
+                    onClick={handleOpenModal}
+                >
                     New Purchase Request
-                </Button> 
+                </Button>
+
+                <Modal
+                    open={openModal}
+                    onClose={handleCloseModal}
+                >
+                    <Box style={{ display: 'flex', margin: 'auto', justifyContent: 'center', width: '70%', height: '100%'}}>
+                        <NewRequestForm handleCloseModal={handleCloseModal}/>
+                    </Box>
+                </Modal> 
             </Stack>
             <Box sx={{ width: '100%', typography: 'body1' }}>
                 <TabContext value={tabValue}>
